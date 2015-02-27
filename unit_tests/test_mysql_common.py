d83756eb9a5abdc2e935ee36816b0431726f4917
Edward Hope-Morley <edward.hope-morley@canonical.com>
2015-02-27 10:17:34 +0000

    synced ch and add unit test

---
+++ unit_tests/test_mysql_common.py
@@ -0,0 +1,17 @@
+import mock
+import unittest
+import sys
+
+sys.modules['MySQLdb'] = mock.Mock()
+import common
+
+
+class CommonTests(unittest.TestCase):
+
+    @mock.patch.object(common, 'MySQLHelper', spec=common.MySQLHelper)
+    def test_get_db_helper(self, mock_helper):
+        h = common.get_db_helper()
+        kwargs = {'rpasswdf_template': '/var/lib/mysql/mysql.passwd',
+                  'upasswdf_template': '/var/lib/mysql/mysql-{}.passwd',
+                  'delete_ondisk_passwd_file': False}
+        mock_helper.assert_called_with(**kwargs)
